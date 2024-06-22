5cfedd0ed885538c56b1672e71654a5d7ec23ad3
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-06-21 21:18:42 -0700

    Create worm.js

---
+++ worm.js
@@ -0,0 +1,29 @@
+class Worm extends Entity {
+    constructor(game, x, y) {
+        super(game, x, y, 'worm', '🐛');
+        this.speed = 0.2;
+        this.moveInterval = null;
+        this.startMoving();
+    }
+
+    startMoving() {
+        this.moveInterval = setInterval(() => {
+            this.move();
+        }, 1000);
+    }
+
+    move() {
+        const angle = Math.random() * Math.PI * 2;
+        this.x += Math.cos(angle) * this.speed;
+        this.y += Math.sin(angle) * this.speed;
+
+        // Keep the worm within the game map
+        this.x = Math.max(0, Math.min(this.x, this.game.ui.gameMap.clientWidth));
+        this.y = Math.max(0, Math.min(this.y, this.game.ui.gameMap.clientHeight));
+    }
+
+    remove() {
+        clearInterval(this.moveInterval);
+        super.remove();
+    }
+}
